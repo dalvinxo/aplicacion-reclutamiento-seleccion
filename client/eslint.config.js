3f3fb4587a74664613e8077f680733757d8e84e1
dalvinxo <dalvingamer@gmail.com>
2024-08-28 22:18:14 -0400

    build: agregando configuracion de pettier+eslint"

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,9 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.eslintrc.cjs'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
